004a15ccbd5ddf6866eaff4e74aa1cb49c400d99
ajgreenwell <greenwan@bc.edu>
2020-04-20 15:37:01 -0400

    Updates

---
+++ precache-manifest.ae28edaed316ac247fe3ceaef6f9ddfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1290e0d63e63c211b0faebb00ad33098",
+    "url": "/creative-project-spotify-manager/index.html"
+  },
+  {
+    "revision": "eb025a775e24cc735629",
+    "url": "/creative-project-spotify-manager/static/css/main.f09a1eb1.chunk.css"
+  },
+  {
+    "revision": "a724e9b6f0d83fa29c9b",
+    "url": "/creative-project-spotify-manager/static/js/2.f545d472.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creative-project-spotify-manager/static/js/2.f545d472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb025a775e24cc735629",
+    "url": "/creative-project-spotify-manager/static/js/main.e12c3871.chunk.js"
+  },
+  {
+    "revision": "f094a4b3f22d7cb85898",
+    "url": "/creative-project-spotify-manager/static/js/runtime-main.5c63947e.js"
+  }
+]);
