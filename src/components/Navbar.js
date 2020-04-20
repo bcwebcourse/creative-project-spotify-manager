9e107a0c98f531b1701ccbfd8542326bba39b6e5
ajgreenwell <greenwan@bc.edu>
2020-04-20 01:16:36 -0400

    added logout button and functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { TimeframeContext } from '../contexts/TimeframeContextProvider';
+import { storeItem } from '../utils/storage';
+import { loginRedirectUrl } from '../utils/auth';
 import publicUrl from '../utils/publicUrl';
 import '../styles/Navbar.css';
 
@@ -12,6 +14,21 @@ function Navbar(props) {
     props.setPage('TopSongs');
   } 
 
+  function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+  async function handleLogout() {
+    storeItem('accessToken', '');
+    storeItem('exprDate', '');
+    storeItem('userGrantedAccess', false);
+    const logoutUrl = 'https://accounts.spotify.com/en/logout';
+    const spotifyLogoutWindow = window.open(logoutUrl, 'Spotify Logout', 'width=100,height=100,top=0,left=0,toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1');
+    await timeout(1300);
+    spotifyLogoutWindow.close()
+    window.location.href = loginRedirectUrl();
+  }
+
   return (
     <nav>
       <div className="title">
@@ -20,15 +37,16 @@ function Navbar(props) {
       </div>
       <button className="top-songs-nav-item" onClick={() => props.setPage('Home')}>Home</button>
       <h2 className="top-songs-nav-item">Your Top Songs of the Past:</h2>
-      <button className="btn" onClick={e => handleTopSongsNavigation(e, 'short_term')}>
+      <button onClick={e => handleTopSongsNavigation(e, 'short_term')}>
         Month
       </button>
-      <button className="btn" onClick={e => handleTopSongsNavigation(e, 'medium_term')}>
+      <button onClick={e => handleTopSongsNavigation(e, 'medium_term')}>
         Six Months
       </button>
-      <button className="btn" onClick={e => handleTopSongsNavigation(e, 'long_term')}>
+      <button onClick={e => handleTopSongsNavigation(e, 'long_term')}>
         Few Years
       </button>
+      <button className="top-songs-nav-item" onClick={handleLogout}>Logout</button>
     </nav>
   );
 }
