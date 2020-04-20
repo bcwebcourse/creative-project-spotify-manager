7d7a8c13d2084e13df15d21ce83c66518c672298
ajgreenwell <greenwan@bc.edu>
2020-04-20 01:28:29 -0400

    cleaning up logout code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,17 @@
 import React, { useContext } from 'react';
 import { TimeframeContext } from '../contexts/TimeframeContextProvider';
-import { storeItem } from '../utils/storage';
-import { loginRedirectUrl } from '../utils/auth';
+import { AuthContext } from '../contexts/AuthContextProvider';
 import publicUrl from '../utils/publicUrl';
 import '../styles/Navbar.css';
 
 function Navbar(props) {
   const { setTimeframe, setTimeframeReadable } = useContext(TimeframeContext);
+  const { logoutUser } = useContext(AuthContext);
 
   function handleTopSongsNavigation(e, timeframe) {
     setTimeframe(timeframe);
     setTimeframeReadable(e.target.innerHTML);
     props.setPage('TopSongs');
-  } 
-
-  function timeout(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-  async function handleLogout() {
-    storeItem('accessToken', '');
-    storeItem('exprDate', '');
-    storeItem('userGrantedAccess', false);
-    const logoutUrl = 'https://accounts.spotify.com/en/logout';
-    const spotifyLogoutWindow = window.open(logoutUrl, 'Spotify Logout', 'width=100,height=100,top=0,left=0,toolbar=1,location=1,directories=1,status=1,menubar=1,scrollbars=1');
-    await timeout(1300);
-    spotifyLogoutWindow.close()
-    window.location.href = loginRedirectUrl();
   }
 
   return (
@@ -46,7 +31,7 @@ function Navbar(props) {
       <button onClick={e => handleTopSongsNavigation(e, 'long_term')}>
         Few Years
       </button>
-      <button className="top-songs-nav-item" onClick={handleLogout}>Logout</button>
+      <button className="top-songs-nav-item" onClick={logoutUser}>Logout</button>
     </nav>
   );
 }
