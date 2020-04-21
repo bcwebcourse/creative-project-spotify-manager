249963bcd39833ce885677d5f42e3851ca92150a
ajgreenwell <greenwan@bc.edu>
2020-04-21 17:15:38 -0400

    Updates

--- precache-manifest.61761b60e4a32232ea75d0952567b546.js
+++ precache-manifest.61761b60e4a32232ea75d0952567b546.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "851a11d8470a92970aa170d1c0491ddd",
+    "revision": "a198ca73b07113c0706fd9fa92329edf",
     "url": "/creative-project-spotify-manager/index.html"
   },
   {
-    "revision": "c8e7ab7608298a2b89a3",
+    "revision": "a5e825282c9555111f2a",
     "url": "/creative-project-spotify-manager/static/css/main.15dfac3b.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/creative-project-spotify-manager/static/js/2.23e46977.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c8e7ab7608298a2b89a3",
-    "url": "/creative-project-spotify-manager/static/js/main.5e4a78db.chunk.js"
+    "revision": "a5e825282c9555111f2a",
+    "url": "/creative-project-spotify-manager/static/js/main.dae91e36.chunk.js"
   },
   {
     "revision": "f094a4b3f22d7cb85898",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/creative-project-spotify-manager/precache-manifest.83bb68b789392c9b425fef4484b73736.js"
+  "/creative-project-spotify-manager/precache-manifest.61761b60e4a32232ea75d0952567b546.js"
 );
 
 self.addEventListener('message', (event) => {
