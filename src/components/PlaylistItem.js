f13087701597ac2fd8968991f964f1a92b61a09a
ajgreenwell <greenwan@bc.edu>
2020-04-20 22:40:31 -0400

    fixing empty playlist image bug

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import '../styles/PlaylistItem.css';
+import publicUrl from '../utils/publicUrl';
 
 function PlaylistItem(props) {
   let playlistName = props.playlist.name.substr(0, 25);
   if (playlistName < props.playlist.name)
     playlistName += '...';
+
   function handlePlayListChange(e, playlistName) {
       // props.setPage(playlistName);
       console.log(playlistName);
-  } 
+  }
+
+  function playlistImage() {
+    if (props.playlist.images.length)
+      return props.playlist.images[0].url;
+    else
+      return publicUrl('/empty-playlist.png');
+  }
 
   return (
     <div className="playlist-obj">
         <img
           className="playlist-thumbnail"
-          src={props.playlist.images[0].url}
+          src={playlistImage()}
           alt="playlist"
           onClick={e => handlePlayListChange(e, props.playlist.name)}
         />
