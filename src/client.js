a23a41b46d4db122a47afa46bf1c032d79f96801
ajgreenwell <greenwan@bc.edu>
2020-04-21 15:37:09 -0400

    undoing unwanted changes from dev

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 export default {
-  client_id: "2405b504876748c0be66ee6a13efea60",
-  client_secret: "10a510e1b91145f8b04e197c64da0cb8",
-  redirect_uri: "http://localhost:3000"
+  client_id: "ede2e752aac9420c8255c01272a9d8ca",
+  client_secret: "ed16805939204892bc95f31eb9c9c5b9",
+  redirect_uri: "https://bcwebcourse.github.io/creative-project-spotify-manager"
 }
