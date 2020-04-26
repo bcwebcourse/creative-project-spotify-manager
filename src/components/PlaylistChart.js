2103f5108d4c50b899ac62087934d1c3b2253bf9
Matthew Scott <scottydotty333@gmail.com>
2020-04-25 17:59:02 -0700

    Photo on Playlist Page

--- src/components/PlaylistChart.js
+++ src/components/PlaylistChart.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect, useContext } from 'react';
 import TopSongsChart from './TopSongsChart';
 import { AuthContext } from '../contexts/AuthContextProvider';
 import { PlaylistContext} from '../contexts/PlaylistContextProvider';
+import '../styles/PlaylistItem.css';
 import '../styles/TopSongs.css';
+import publicUrl from '../utils/publicUrl';
 
 function PlaylistChart() {
   const [songs, setSongs] = useState([]);
@@ -29,12 +31,23 @@ function PlaylistChart() {
       setIsAscending(false);
     fetchPlayListTracks();
     }, [accessToken, authenticateUser, playlist.tracks.href]);
+  function playlistImage() {
+    if (playlist.images.length)
+        return playlist.images[0].url;
+    else
+        return publicUrl('/empty-playlist.png');
+    }
   return (
     <div className="top-songs">
       <header className="top-songs-small-header">
         <h2 className="top-songs-title">{ playlist.name }</h2>
       </header>
       <header className="top-songs-large-header">
+        <img
+          className="playlist-thumbnail"
+          src={playlistImage()}
+          alt="playlist"
+        />
         <h2 className="top-songs-title">{ playlist.name }</h2>
       </header>
       {songs.length ?
